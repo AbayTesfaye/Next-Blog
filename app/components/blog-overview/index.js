a9c310e3807efe8c5fb5f4e3553303417b7ade1c
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-24 23:43:13 +0300

    handle Add New Blog button

--- app/components/blog-overview/index.js
+++ app/components/blog-overview/index.js
@@ -1,10 +1,64 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Button } from "../../../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 export default function BlogOverview() {
+  const [openBlogDialog, setOpenBlogDialog] = useState(false);
   return (
-    <div>
-        <h1>This is blog overview!</h1>
+    <div className="min-h-screen flex flex-col gap-10 py-4 px-6 bg-gradient-to-r from-purple-400 to to-blue-600">
+      <div>
+        <Button onClick={() => setOpenBlogDialog(true)}>Add New Blog</Button>
+      </div>
+      <div>
+        <h1>This is blog listing section</h1>
+        <Dialog open={openBlogDialog} onOpenChange={setOpenBlogDialog}>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
-  )
+  );
 }
